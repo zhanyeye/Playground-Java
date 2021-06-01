e88df438aa62b2e527afffe5dbc9b117cc4de71a
zhanyeye <zhanyeye@qq.com>
2021-06-01 11:06:07 +0800

    Initial commit

---
+++ src/main/java/cn/zhanyeye/App.java
@@ -0,0 +1,13 @@
+package cn.zhanyeye;
+
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello, this is my playground");
+    }
+}
