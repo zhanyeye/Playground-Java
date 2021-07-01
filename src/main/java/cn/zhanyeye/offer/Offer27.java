2df6ec81e3c072ed0ea37935c91fc9a20881f99a
zhanyeye <zhanyeye@qq.com>
2021-07-01 21:38:59 +0800

    Update Offer27.java

--- src/main/java/cn/zhanyeye/offer/Offer27.java
+++ src/main/java/cn/zhanyeye/offer/Offer27.java
@@ -1,9 +1,19 @@
 package cn.zhanyeye.offer;
 
+import cn.zhanyeye.common.TreeNode;
+
 /**
+ * https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
  * @Author zhanyeye
  * @Description
  * @Date 28/06/2021
  **/
 public class Offer27 {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode mirrorTree = new TreeNode(root.val);
+        mirrorTree.left = mirrorTree(root.right);
+        mirrorTree.right = mirrorTree(root.left);
+        return mirrorTree;
+    }
 }
