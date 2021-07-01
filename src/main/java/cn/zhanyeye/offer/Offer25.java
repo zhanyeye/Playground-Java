f5020435f6879866863840a6849cc668f27968bc
zhanyeye <zhanyeye@qq.com>
2021-07-01 14:45:44 +0800

    Update Offer25.java

--- src/main/java/cn/zhanyeye/offer/Offer25.java
+++ src/main/java/cn/zhanyeye/offer/Offer25.java
@@ -1,9 +1,28 @@
 package cn.zhanyeye.offer;
 
+import cn.zhanyeye.common.ListNode;
+
 /**
+ * https://leetcode-cn.com/problems/he-bing-liang-ge-pai-xu-de-lian-biao-lcof/
  * @Author zhanyeye
  * @Description
  * @Date 28/06/2021
  **/
 public class Offer25 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = new ListNode(l1.val);
+                l1 = l1.next;
+            } else {
+                cur.next = new ListNode((l2.val));
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = l1 != null ? l1 : l2;
+        return head.next;
+    }
 }
